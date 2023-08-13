bdc117e008833c28513d6efe266a07d5bad90b5c
rainbow2414 <rainbow@ngs.ru>
2023-08-13 13:29:16 +0700

    Добавили решение задачи 10

---
+++ Ex_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+using System.Xml.XPath;
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int SecondDigit(int number)
+{
+    return (number / 10 ) % 10;
+}
+
+int num = InputNum("Введите трёхзначное число:");
+Console.WriteLine($"Вторая цифра числа {num} равна {SecondDigit(num)}");
